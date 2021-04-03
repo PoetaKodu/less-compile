410b21a1a885f34c7de1b29a94a6005b5a57f119
PoetaKodu <rfpawelsyska@gmail.com>
2021-04-03 04:31:36 +0200

    Implemented basic compilation script.

---
+++ bin/Compile.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+const { promisify } = require("util");
+const glob = promisify(require("glob"));
+const exec = promisify(require("child_process").exec);
+
+async function main()
+{
+	if (process.argv.length < 3)
+		throw "invalid parameter";
+
+	const {err, files} = await glob(process.argv[2]);
+	
+	if (err)
+		throw "Could not list .less files.";
+
+	const promises = [];
+
+	// Start compiling each file.
+	for(let i in files)
+	{
+		const inFile 	= files[i];
+		const outFile 	= inFile.substr(0, inFile.lastIndexOf(".")) + ".css";
+
+		console.log(`# [${i + 1}/${files.length}] Compiling: "${inFile}" -> "${outFile}"`);
+
+		const prom = exec(`lessc "${inFile}" "${outFile}"`);
+		promises.push(prom);
+	}
+
+	// Wait for all compilations to end.
+	await Promise.all(promises);
+
+	console.log(`# Finished compiling ${promises.length} files.`);
+
+	return 0;
+}
+
+try {
+	return await main();
+}
+catch(exc) {
+	console.error(`Error: an exception occurred.\nDetails: "${exc}"`);
+}
